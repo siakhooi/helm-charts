a91d877b34a26bcd22f5594d16d5f192773fd23e
Siak Hooi <siakhooi@gmail.com>
2024-09-23 00:42:39 +0000

    refactor scripts to follow google style

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,6 @@ echo "$GPG_PRIVATE_KEY" | gpg --import
 gpg --list-keys
 
 find docs -name '*.tgz' | while read -r filename; do
-    helm-sign --key "$GPG_KEY_NAME" "$filename"
+  helm-sign --key "$GPG_KEY_NAME" "$filename"
 done
 helm repo index docs --url https://siakhooi.github.io/helm-charts
--- scripts/gpg-generate-key.sh
+++ scripts/gpg-generate-key.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-GPG_KEY_NAME=siakhooi-helm
-GPG_KEY_BATCH=./scripts/gpg.batch
+readonly GPG_KEY_NAME=siakhooi-helm
+readonly GPG_KEY_BATCH=./scripts/gpg.batch
+readonly GPG_KEY_DIRECTORY=./keys
 
-GPG_KEY_DIRECTORY=./keys
-
-WORKDIR=$(mktemp -d)
-export GNUPGHOME="$WORKDIR"
+working_directory=$(mktemp -d)
+export GNUPGHOME="$working_directory"
+readonly GNUPGHOME
 
 GPG_PUBLIC_KEY_ASC=${GPG_KEY_DIRECTORY}/${GPG_KEY_NAME}.gpg.public.asc
 GPG_PUBLIC_KEY_BIN=${GPG_KEY_DIRECTORY}/${GPG_KEY_NAME}.gpg.public.key
 GPG_PRIVATE_KEY_ASC=${GPG_KEY_DIRECTORY}/${GPG_KEY_NAME}.gpg.private.asc
 GPG_PRIVATE_KEY_BIN=${GPG_KEY_DIRECTORY}/${GPG_KEY_NAME}.gpg.private.key
 
-mkdir -p $GPG_KEY_DIRECTORY
+mkdir -p -v $GPG_KEY_DIRECTORY
 (
     set -x
     gpg --no-tty --batch --gen-key "$GPG_KEY_BATCH"
-
     gpg --armor --export "${GPG_KEY_NAME}" >"$GPG_PUBLIC_KEY_ASC"
     gpg --export "${GPG_KEY_NAME}" >"$GPG_PUBLIC_KEY_BIN"
     gpg --armor --export-secret-keys "${GPG_KEY_NAME}" >"$GPG_PRIVATE_KEY_ASC"
